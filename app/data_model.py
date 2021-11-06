5a041815990ecdbb78d9a263bb40f62c1879d7a9
Ben Holden <benjamin.p.holden@gmail.com>
2021-11-06 12:19:10 -0600

    Add coments to helpers and data_model

--- app/data_model.py
+++ app/data_model.py
@@ -5,8 +5,12 @@ from sklearn.model_selection import train_test_split
 
 from helpers import process_data
 
-x, y = process_data("data.csv", outcomes=True)
-x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2)
-model = LogisticRegression(C=1.0)
-model.fit(x_train, y_train)
-output_model = pickle.dump(model, open("model.pkl", "wb"))
+x, y = process_data("data.csv", outcomes=True) #collects data from panda data tables
+
+x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2) #splits the data into training and validation values
+
+model = LogisticRegression(C=1.0) #creates a new model
+
+model.fit(x_train, y_train) #fits the model to the data
+
+output_model = pickle.dump(model, open("model.pkl", "wb")) #exports the model to a file that is called when processing new data
--- app/helpers.py
+++ app/helpers.py
@@ -7,35 +7,41 @@ from sklearn.linear_model import LogisticRegression
 
 
 def process_data(filename: str | SpooledTemporaryFile, outcomes: bool = False) -> pd.DataFrame:
-    """[summary]
+    """Parse and format .csv file into a panda DataFrame
 
     Args:
-        filename (str): [description]
-        outcomes (bool, optional): [description]. Defaults to False.
+        filename (str | SpooledTemporaryFile): .csv file to upload
+        outcomes (bool): Bool that represents if the outcomes should be returned. Defaults to False.
 
     Returns:
-        pd.DataFrame: [description]
+        pd.DataFrame: All data in the uploaded .csv file that contibutes to
+                        potential diabetes
     """
-    df = pd.read_csv(filename, na_values="")
+   
+    
+    df = pd.read_csv(filename, na_values="") #Store the uploaded .csv file in a DataFrame
 
-    df = df.drop(columns=["Unnamed: 9", "Patient"])
-    df = df[df["Outcome"].notna()]
+    df = df.drop(columns=["Unnamed: 9", "Patient"]) #Drop the empty data column and the patient number column from the DataFrame
+    df = df[df["Outcome"].notna()] #Drop the rows when the outcome is NaN 
 
-    x = df.drop(columns="Outcome")
-    y = df["Outcome"]
-    x = (x - x.mean()) / x.std()
+    x = df.drop(columns="Outcome") #Store all the factors that affect diabetes chance in x
+    y = df["Outcome"] #Store the outcome column in y
+    x = (x - x.mean()) / x.std() #Normalizes the values of x
 
-    return x, y if outcomes else x
+    return x, y if outcomes else x  #return either both the contributing factors and the outcomes if outcomes == True
+                                                #else, return the contributing factors  
 
 
 def get_predictions(x: pd.DataFrame, model: LogisticRegression) -> pd.Series:
-    """[summary]
+    """The function uses the data from the panda tables that doesn't contain outcomes
+        and uses the model that was exported from data_model to predict whether or not 
+        a patient has diabetes or not
 
     Args:
-        x (pd.DataFrame): [description]
-        model (LogisticRegression): [description]
+        x (pd.DataFrame): Inputted framework of data obtained form the .csv file
+        model (LogisticRegression): Model obtained from a test set of data, which was run through a logistics regression
 
     Returns:
-        pd.Series: [description]
+        pd.Series: Outputs the panda array of the prediction of whether the patient has diabetes or not
     """
-    ...
+    return model.predict(x)
