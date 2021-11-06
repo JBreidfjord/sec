48352fdf58b49fc8ae7336668474ebd36d355552
HitenMahal <72403820+HitenMahal@users.noreply.github.com>
2021-11-06 11:56:54 -0600

    Update app.js

--- app/static/app.js
+++ app/static/app.js
@@ -3,25 +3,5 @@
 
 document.getElementById("file_submit").onclick = async function() {
     var csv_file = document.getElementById("file_input").files[0];
-    // const rawData = await csv_file.text();
-
-    // let data_array = rawData.split("\r\n");
-    // let header = data_array[0].split(",");
-    // let rows = data_array.length;
-    // let cols = header.length;
-    // let jsonData = [];
-
-    // for(i=1; i <rows-1; i++) {
-    //     let obj = {};
-    //     let myNewLine = data_array[i].split(",");
-    //     for (j=0; j < cols; j++) {
-    //         obj[header[j]] = myNewLine[j];
-    //     };
-    //     jsonData.push(obj)
-    // };
-
-    // document.getElementById("csv").innerHTML = data_array;
-    // console.log(jsonData);
-    // console.table(jsonData);
     console.log(csv_file)
 }
