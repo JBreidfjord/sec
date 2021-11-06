003d2ed1f03be259ddc74ccaf5ca2ecc4ad0a305
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-11-06 10:49:27 -0600

    Fix function name & update requirements

--- app/helpers.py
+++ app/helpers.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
-def processed_data(filename: str):
-    df = pd.read_csv(filename, na_values='')
 
-    df = df.drop(columns=['Unnamed: 9', 'Patient'])
-    df = df[df['Outcome'].notna()]
+def process_data(filename: str):
+    df = pd.read_csv(filename, na_values="")
+
+    df = df.drop(columns=["Unnamed: 9", "Patient"])
+    df = df[df["Outcome"].notna()]
 
     X = df.drop(columns="Outcome")
     y = df["Outcome"]
