c8a1f7f5c734fb8836322f0eab72372f7682e49b
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-11-06 11:37:44 -0600

    Define post response & create docstring models

--- app/helpers.py
+++ app/helpers.py
@@ -3,15 +3,36 @@ from __future__ import annotations
 from tempfile import SpooledTemporaryFile
 
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
 
 
-def process_data(filename: str | SpooledTemporaryFile) -> tuple[pd.DataFrame, pd.Series]:
+def process_data(filename: str | SpooledTemporaryFile) -> pd.DataFrame:
+    """[summary]
+
+    Args:
+        filename (str | SpooledTemporaryFile): [description]
+
+    Returns:
+        tuple[pd.DataFrame, pd.Series]: [description]
+    """
     df = pd.read_csv(filename, na_values="")
 
     df = df.drop(columns=["Unnamed: 9", "Patient"])
     df = df[df["Outcome"].notna()]
 
-    X = df.drop(columns="Outcome")
-    y = df["Outcome"]
-    X = (X - X.mean()) / X.std()
-    return X, y
+    x = df.drop(columns="Outcome")
+    x = (x - x.mean()) / x.std()
+    return x
+
+
+def get_predictions(x: pd.DataFrame, model: LogisticRegression) -> pd.Series:
+    """[summary]
+
+    Args:
+        x (pd.DataFrame): [description]
+        model (LogisticRegression): [description]
+
+    Returns:
+        pd.Series: [description]
+    """
+    ...
--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 
-from app.helpers import process_data
+from app.helpers import get_predictions, process_data
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
@@ -15,4 +15,5 @@ async def root():
 
 @app.post("/")
 async def upload_file(file: UploadFile = File(...)):
-    return {"filename": file.filename}
+    x = process_data(file.file)
+    return get_predictions(x)
