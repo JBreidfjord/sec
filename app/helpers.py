40abe49c67f4a207062f2e849818d4385aa38af1
Ben Holden <benjamin.p.holden@gmail.com>
2021-11-06 09:57:04 -0600

    Add Data Parsing Function

---
+++ app/helpers.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+def data_with_outcomes(filename: str):
+    df = pd.read_csv(filename, na_values='')
+
+    df = df.drop(columns=['Unnamed: 9', 'Patient'])
+    df = df[df['Outcome'].notna()]
+
+    X = df.drop(columns="Outcome")
+    y = df["Outcome"]
+    
+    return X, y
