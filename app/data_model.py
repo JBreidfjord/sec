cfaa4ef64bba961fd6720ee96c954f3983c4fc9c
Jonathan Breidfjord <jbreidfjord@gmail.com>
2021-11-06 13:15:39 -0600

    Format comments

--- app/data_model.py
+++ app/data_model.py
@@ -5,12 +5,17 @@ from sklearn.model_selection import train_test_split
 
 from helpers import process_data
 
-x, y = process_data("data.csv", outcomes=True) #collects data from panda data tables
+# Collects data from panda data tables
+x, y = process_data("data.csv", outcomes=True)
 
-x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2) #splits the data into training and validation values
+# Splits the data into training and validation values
+x_train, x_val, y_train, y_val = train_test_split(x, y)
 
-model = LogisticRegression(C=1.0) #creates a new model
+# Creates a new model
+model = LogisticRegression(C=1.0)
 
-model.fit(x_train, y_train) #fits the model to the data
+# Fits the model to the data
+model.fit(x_train, y_train)
 
-output_model = pickle.dump(model, open("model.pkl", "wb")) #exports the model to a file that is called when processing new data
+# Exports the model to a file that is called when processing new data
+output_model = pickle.dump(model, open("model.pkl", "wb"))
